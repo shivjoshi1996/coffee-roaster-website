df7fc8f570d1eb9fcc8ab0df5278653a7be0b321
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-03-13 22:54:56 -0800

    WIP Mobile nav toggle, accordion logic

---
+++ js/mobile-nav.js
@@ -0,0 +1,14 @@
+// Mobile Nav 
+
+// Hamburger
+
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("mobile-nav-active").style.display = "block";
+})
+
+
+// Close Button
+
+document.getElementById("close-button").addEventListener("click", function() {
+    document.getElementById("mobile-nav-active").style.display = "none";
+})
