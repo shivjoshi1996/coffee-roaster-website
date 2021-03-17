72e171c8eef1d32d8389947e22e5c726daf1bd42
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-03-17 11:08:58 -0700

    WIP plan logic

--- js/accordion.js
+++ js/accordion.js
@@ -47,11 +47,62 @@ for (var x = 0; x < accordionOption.length; x++) {
       this.classList.toggle("option-active");
       console.log(this.children[0].textContent);
       // console.log(this.parentElement);
+      console.log(this.children);
+
+      // if capsule is selected, hide accordion 4
 
       if (this.children[0].textContent === "Capsule") {
-        document.querySelector(".accordian-4").style.display = "none";
+        document.querySelector(".accordion-4").style.display = "none";
+      } else {
+        document.querySelector(".accordion-4").style.display = "block";
+      }
+
+
+      // Strings for order summary 
+
+      // How Do you Drink your Coffee
+
+      let howDoYouDrinkCoffee = "";
+
+      if (this.children[0].textContent === "Capsule") {
+        howDoYouDrinkCoffee = "using Capsules";
+      } else if (this.children[0].textContent === "Filter") {
+        howDoYouDrinkCoffee = "as Filter";
+      } else if (this.children[0].textContent === "Espresso") {
+        howDoYouDrinkCoffee = "as Espresso";
       }
       
+      console.log(howDoYouDrinkCoffee);
+
+      // What Type of Coffee
+
+      let typeOfBean = "";
+
+      if (this.children[0].textContent === "Single origin") {
+        typeOfBean = "Single origin";
+      } else if (this.children[0].textContent === "Decaf") {
+        typeOfBean = "Decaf";
+      } else if (this.children[0].textContent === "Blended") {
+        typeOfBean = "Blended";
+      }
+      
+      console.log(typeOfBean);
+
+      // How much would you like
+
+      let howMuchWouldYouLike = "";
+
+      if (this.children[0].textContent === "250g") {
+        howMuchWouldYouLike = "250g";
+      } else if (this.children[0].textContent === "500g") {
+        howMuchWouldYouLike = "500g";
+      } else if (this.children[0].textContent === "1000g") {
+        howMuchWouldYouLike = "1000g";
+      }
+      
+      console.log(howMuchWouldYouLike);
+
+        
     }
 
   });
